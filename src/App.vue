7325dc41b3ea69d2ce719711e9e91edb6e36765c
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-11-13 22:36:16 +0300

    add materialize-css and turn in

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <button class="btn">Click</button>
     <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
+</style>
