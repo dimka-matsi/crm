86eb516c169cc99b548f8796f76b08f9ea819807
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-11-13 23:08:40 +0300

    add new components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="app">
-    <button class="btn">Click</button>
-    <router-view/>
+    <component v-bind:is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
+
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
+export default {
+  components: {
+    EmptyLayout,
+    MainLayout
+  },
+  computed: {
+    layout () {
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
